c3ca50b75375d6c4432b25682616aaa3f50e8c50
zwen <zuwendri32@gmail.com>
2025-09-24 18:00:40 +0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
 var side = document.getElementById("aside");
-const rndom = Math.floor(Math.random() * 1000) + 501;
-
-
-
-
+var body = document.getElementById("body")
+const rndom = Math.floor(Math.random() * 1100) + 401;
 
 btn1.style.fontSize ="20px"
   if (btn1.style.fontSize === "20px") {
@@ -13,6 +10,7 @@ btn1.style.fontSize ="20px"
       side.style.display = "block"
       btn1.style.fontSize = "1px"
       btn1.style.animation = "none"
+      
       setTimeout(function (){
         btn2.style.animation = "btn2 1s ease"
         console.log(rndom)
@@ -26,10 +24,10 @@ btn1.style.fontSize ="20px"
          btn1.style.animation = ".5s btn1 ease"
          console.log(rndom)
          clearTimeout(rndom)
+         Cache 
         })
       } 
     })
-    
   }
   // } else {
   //   btn2.addEventListener("click", function () {
